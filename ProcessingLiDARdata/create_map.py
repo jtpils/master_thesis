b1e79c5519796cda73aed4621e508fa1f1bfb49b
sablind <sablind@users.noreply.github.com>
2019-02-14 16:25:21 +0100

    The file creates the discretized map and saves the channel features in a folder

--- ProcessingLiDARdata/create_map.py
+++ ProcessingLiDARdata/create_map.py
@@ -1,38 +1,44 @@
 import numpy as np
 from lidar_data_functions import *
 from map_functions import *
-from PIL import Image
-import matplotlib.pyplot as plt
 
-path_to_csv = '/home/master04/Desktop/_out/_out_Town03_190207_18/Town03_190207_18.csv'
-path_to_ply_folder = '/home/master04/Desktop/_out/_out_Town03_190207_18/pc/'
+path_to_ply_folder = '/home/master04/Desktop/_out/_out_Town02_190208_1/pc/'
+path_to_csv = '/home/master04/Desktop/_out/_out_Town02_190208_1/Town02_190208_1.csv'
 
 # get list of all ply files in the ply_folder.
 files_in_ply_folder = os.listdir(path_to_ply_folder)
 
 pc_dict = {}
+pc_super_array = np.zeros((1, 3))
+print(np.shape(pc_super_array))
 max_x_val = float("-inf")
 max_y_val = float("-inf")
 min_x_val = float("inf")
 min_y_val = float("inf")
-
 i = 0
 
-channel_matrix = np.zeros([4, 600, 600])
-for file in files_in_ply_folder[:1]:
-    i = i+1
+for file in files_in_ply_folder[0:500:5]:  # the last number is how large steps to take
+
+    # i = i+1  # this parameter is used if storing each ply file in a dict
+
+    # Create the path to the ply file
     path_to_ply = path_to_ply_folder + file
     point_cloud, global_coordinates = load_data(path_to_ply, path_to_csv)
 
     # rotate, translate the point cloud to global coordinates and trim the point cloud
-    trimmed_pc = trim_pointcloud(point_cloud, range=50, roof=10, floor=-3)
-    rotated_pc = rotate_pointcloud_to_global(trimmed_pc, global_coordinates)
+    trimmed_pc = trim_pointcloud(point_cloud, range=10, roof=10, floor=-3)
+    rotated_pc = rotate_pointcloud_to_global(point_cloud, global_coordinates)
     rotated_and_translated_pc = translate_pointcloud_to_global(rotated_pc, global_coordinates)
 
-    # save the trimmed pc into a dictionary
-    pc_dict["pc{0}".format(i)] = rotated_and_translated_pc
+    # concatenate the rotated and translated array into a super array
+    pc_super_array = np.concatenate((pc_super_array, rotated_and_translated_pc))
+
+    # Here one can save the trimmed pc into a dict
+    # pc_dict["pc{0}".format(i)] = rotated_and_translated_pc
 
     # find max and min value of the x and y coordinates
+    # If using the super array this can be removed and the max and min values can be found from the super_array outside
+    # the for loop.
     max_x_val_tmp = np.max(rotated_and_translated_pc[:, 0])
     min_x_val_tmp = np.min(rotated_and_translated_pc[:, 0])
 
@@ -41,24 +47,27 @@ for file in files_in_ply_folder[:1]:
 
     if max_x_val_tmp > max_x_val:
         max_x_val = max_x_val_tmp
-        # print('max x', max_x_val)
 
     if min_x_val_tmp < min_x_val:
         min_x_val = min_x_val_tmp
-        # print('min x', min_x_val)
 
     if max_y_val_tmp > max_y_val:
         max_y_val = max_y_val_tmp
-        # print('max y', max_y_val)
 
     if min_y_val_tmp < min_y_val:
         min_y_val = min_y_val_tmp
-        # print('min y', min_y_val)
 
-    # discretize the cells in the map
 
-    # channel_matrix = channel_matrix + discretize_pointcloud(rotated_and_translated_pc, spatial_resolution=0.05)
+# delete the zeros in the first row in the super array that was used at the initalization of the array.
+pc_super_array = np.delete(pc_super_array, 0, axis=0)
 
-# array_to_png(channel_matrix)
-# print(' max x', max_x_val, '\n', 'max y', max_y_val, '\n', 'min x', min_x_val, '\n', 'min y', min_y_val )
+# save the max and min values in an array. This is used to decide the size of the map
+min_max = np.array((min_x_val, max_x_val, min_y_val, max_y_val))
+
+# discretice the point cloud.
+discretized_pc = discretize_pointcloud_map(pc_super_array, min_max)
+
+# Save the discretized map in a folder.
+array_to_png(discretized_pc)
+
 
