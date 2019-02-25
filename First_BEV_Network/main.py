3b0a563acfb2a08dcce91f11df7560eefaa65d45
sablind <annlund@users.noreply.github.com>
2019-02-25 09:05:53 +0100

    testing GPU availability

--- First_BEV_Network/main.py
+++ First_BEV_Network/main.py
@@ -25,8 +25,10 @@ print('Number of GPUs available: ', torch.cuda.device_count())
 # CNN = FirstBEVNet()
 CNN = SuperSimpleCNN()
 CNN = CNN.cuda()
-print('Are model parameters on CUDA? ', next(CNN.parameters()).is_cuda())
 print('Are model parameters on CUDA? ', CNN.cuda())
+print('Are model parameters on CUDA? ', next(CNN.parameters()))#.is_cuda())
+print(type(next(CNN.parameters())))
+
 
 # create directory for model weights
 current_path = os.getcwd()
