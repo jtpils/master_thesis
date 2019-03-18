0e6207b82d7d55d3059aa929359ac32189d4cf13
sablind <annlund@users.noreply.github.com>
2019-03-18 10:03:58 +0100

    new test on cloud

--- First_BEV_Network/main.py
+++ First_BEV_Network/main.py
@@ -28,9 +28,9 @@ path_training_data = '/home/master04/Desktop/Dataset/fake_training_set' #
 path_validation_data = '/home/master04/Desktop/Dataset/fake_validation_set' #
 path_test_data = '/home/master04/Desktop/Dataset/fake_test_set' #
 
-#path_training_data = '/home/annika_lundqvist144/Dataset/fake_training_set' #input('Path to training data set folder: ')
-#path_validation_data = '/home/annika_lundqvist144/Dataset/fake_validation_set' #input('Path to validation data set folder: ')
-#path_test_data = '/home/annika_lundqvist144/Dataset/fake_test_set' #input('Path to test data set folder: ')
+path_training_data = '/home/annika_lundqvist144/Dataset/fake_training_set' #input('Path to training data set folder: ')
+path_validation_data = '/home/annika_lundqvist144/Dataset/fake_validation_set' #input('Path to validation data set folder: ')
+path_test_data = '/home/annika_lundqvist144/Dataset/fake_test_set' #input('Path to test data set folder: ')
 
 batch_size = int(input('Input batch size: '))
 plot_flag = 'n' #input('Plot results? y / n: ')
