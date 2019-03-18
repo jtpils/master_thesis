c03af65521e0b39c7402c978bacead86bc4d4317
sablind <sablind@users.noreply.github.com>
2019-03-18 08:29:56 +0100

    nothing new

--- point_cloud_input/main_annika.py
+++ point_cloud_input/main_annika.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import time
 
-
+'''
 path = '/home/master04/Desktop/Dataset/Town02_sorted_grid_ply/global_coordinates.csv'
 
 t1 = time.time()
@@ -23,16 +23,30 @@ t4 = time.time()
 print('Time to read .npy file with np: ', t4-t3)
 
 '''
+
+
+
+
 print('Initializing data set: ')
-data_path = '/Users/annikal/Desktop/Ply_files/TEST_sorted_grid_ply'
-dataset = PointCloudDataSet(data_set_path=data_path, number_of_samples=1)
+data_path = '/Users/sabinalinderoth/Desktop/Ply_files/TEST_sorted_grid_ply'
+
+
+
+dataset = PointCloudDataSet(data_set_path=data_path, number_of_samples=100)
 print('Done initializing data set. ')
 print(' ')
 
 print('Creating samples. ')
-idx = 0
-sample_dict = dataset.__getitem__(idx)
+#idx = 0
+t1 = time.time()
+for idx in tqdm(range(0,100)):
 
+    sample_dict = dataset.__getitem__(idx)
+
+t2 = time.time()
+print('Time:', t2-t1)
+
+'''
 print('discretizing sweep: ')
 sweep = discretize_pointcloud(sample_dict['sweep'], array_size=60, trim_range=15, spatial_resolution=0.5, padding=False)
 print(' ')
@@ -47,5 +61,4 @@ print('Creating png: ')
 array_to_png(cutout, 'map_png')
 del cutout
 '''
-
 
