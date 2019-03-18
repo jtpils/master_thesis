ed8d39878b1c3b641dcc5bde48d579b9019e5569
sablind <annlund@users.noreply.github.com>
2019-03-18 16:04:09 +0100

    H

--- First_BEV_Network/main.py
+++ First_BEV_Network/main.py
@@ -12,7 +12,7 @@ from new_networks import *
 # /Users/annikal/Desktop/fake_training_data_trans2
 
 # load old weights! change here manually
-load_weights = True
+load_weights = False
 load_weights_path = '/home/annika_lundqvist144/master_thesis/First_BEV_Network/param_190318_2/parameters/epoch_1_checkpoint.pt'
 
 model_name = input('Type name of new folder: ')
