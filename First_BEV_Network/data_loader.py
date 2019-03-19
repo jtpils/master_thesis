0d31467c043d336c63a7f980fb6d39f2c0fe17ac
sablind <annlund@users.noreply.github.com>
2019-03-19 18:33:02 +0100

    h

--- First_BEV_Network/data_loader.py
+++ First_BEV_Network/data_loader.py
@@ -21,7 +21,7 @@ def get_loaders(path_training_data, path_validation_data, path_test_data, batch_
     n_training_samples = 50  #len(training_data_set)
     print('Number of training samples: ', n_training_samples)
     train_sampler = SubsetRandomSampler(np.arange(1, n_training_samples+1, dtype=np.int64))
-    train_loader = torch.utils.data.DataLoader(training_data_set, batch_size=batch_size, sampler=train_sampler, num_workers=8, **kwargs)
+    train_loader = torch.utils.data.DataLoader(training_data_set, batch_size=batch_size, sampler=train_sampler, num_workers=4, **kwargs)
     #train_loader = torch.utils.data.DataLoader(training_data_set, batch_size=batch_size, shuffle=True, num_workers=8, **kwargs)
 
     # Validation
