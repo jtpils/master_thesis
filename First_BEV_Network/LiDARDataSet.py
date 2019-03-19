594c34ecdce12765a5ab707b6f8d0366fbc73d58
sablind <annlund@users.noreply.github.com>
2019-03-19 17:11:25 +0100

    h

--- First_BEV_Network/LiDARDataSet.py
+++ First_BEV_Network/LiDARDataSet.py
@@ -42,4 +42,5 @@ class LiDARDataSet(Dataset):
         training_sample = {'sample': sample, 'labels': labels.values}  #  This worked on Sabinas Mac.
         # training_sample = {'sample': sample, 'labels': labels.to_numpy()}
 
+        del labels, labels_csv, sample, sample_file
         return training_sample
