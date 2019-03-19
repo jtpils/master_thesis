fd72035c408888f25989a5bd5d3e016d6da1154c
sablind <annlund@users.noreply.github.com>
2019-03-19 18:36:23 +0100

    h

--- First_BEV_Network/train_network.py
+++ First_BEV_Network/train_network.py
@@ -105,7 +105,7 @@ def train_network(n_epochs, learning_rate, patience, folder_path, device, use_cu
             labels = data['labels']
 
             if use_cuda:
-                sample, labels = sample.cuda(async=True), labels.cuda(async=True)
+                #sample, labels = sample.cuda(async=True), labels.cuda(async=True)
                 #sample, labels = sample.cuda(), labels.cuda()
             sample, labels = Variable(sample), Variable(labels)
             # Wrap them in a Variable object
