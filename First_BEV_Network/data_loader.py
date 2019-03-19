0c1f38ac8eafef20d72d5617cd911a29149016fd
sablind <annlund@users.noreply.github.com>
2019-03-19 17:43:03 +0100

    h

--- First_BEV_Network/data_loader.py
+++ First_BEV_Network/data_loader.py
@@ -18,7 +18,7 @@ def get_loaders(path_training_data, path_validation_data, path_test_data, batch_
     #test_data_set = LiDARDataSet(csv_file, sample_dir, use_cuda)
 
     # Training
-    n_training_samples = 200 #len(training_data_set)
+    n_training_samples = 160 #len(training_data_set)
     print('Number of training samples: ', n_training_samples)
     train_sampler = SubsetRandomSampler(np.arange(1, n_training_samples+1, dtype=np.int64))
     train_loader = torch.utils.data.DataLoader(training_data_set, batch_size=batch_size, sampler=train_sampler, num_workers=8, **kwargs)
