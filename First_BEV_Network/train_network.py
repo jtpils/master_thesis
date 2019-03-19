75c7f70ed4746a46e193f3cbbe344d342bdb8194
sablind <annlund@users.noreply.github.com>
2019-03-19 11:08:36 +0100

    h

--- First_BEV_Network/train_network.py
+++ First_BEV_Network/train_network.py
@@ -121,7 +121,7 @@ def train_network(n_epochs, learning_rate, folder_path, device, use_cuda, batch_
                 running_loss = 0.0
                 time_epoch = time.time()
         
-            del data, sample, labels, outputs, loss_size
+            #del data, sample, labels, outputs, loss_size
 
         # At the end of the epoch, do a pass on the validation set
         total_val_loss = 0
@@ -144,7 +144,7 @@ def train_network(n_epochs, learning_rate, folder_path, device, use_cuda, batch_
                 val_loss_size = loss(val_outputs, labels.float())
                 total_val_loss += val_loss_size.item()
 
-                del data, sample, labels, val_outputs, val_loss_size
+                #del data, sample, labels, val_outputs, val_loss_size
 
         print("Training loss: {:.4f}".format(total_train_loss / len(train_loader)),
               ", Validation loss: {:.4f}".format(total_val_loss / len(val_loader)),
