2646a9430f6ef8dcee5377e54d2c626279240a12
sablind <annlund@users.noreply.github.com>
2019-03-20 07:56:36 +0100

    new cleaned up directory for training BEV images

--- BEV_network/data_loader.py
+++ BEV_network/data_loader.py
@@ -4,7 +4,7 @@ from torch.utils.data.sampler import SubsetRandomSampler
 from LiDARDataSet import LiDARDataSet
 
 
-def get_loaders(path_training_data, path_validation_data, path_test_data, batch_size, use_cuda, kwargs):
+def get_loaders(path_training_data, path_validation_data, batch_size, use_cuda, kwargs):
     csv_file = path_training_data + '/labels.csv'
     sample_dir = path_training_data + '/samples/'
     training_data_set = LiDARDataSet(csv_file, sample_dir, use_cuda)
@@ -12,10 +12,6 @@ def get_loaders(path_training_data, path_validation_data, path_test_data, batch_
     #csv_file = path_validation_data + '/labels.csv'
     #sample_dir = path_validation_data + '/samples/'
     #validation_data_set = LiDARDataSet(csv_file, sample_dir, use_cuda)
-
-    #csv_file = path_test_data + '/labels.csv'
-    #sample_dir = path_test_data + '/samples/'
-    #test_data_set = LiDARDataSet(csv_file, sample_dir, use_cuda)
 
     # Training
     n_training_samples = 160  #len(training_data_set)
@@ -29,12 +25,6 @@ def get_loaders(path_training_data, path_validation_data, path_test_data, batch_
     #print('Number of validation samples: ', n_val_samples)
     #val_sampler = SubsetRandomSampler(np.arange(1, n_val_samples+1, dtype=np.int64))
     #val_loader = torch.utils.data.DataLoader(validation_data_set, batch_size=batch_size, sampler=val_sampler, num_workers=4, **kwargs)
-
-    # Test
-    #n_test_samples = len(test_data_set)
-    #print('Number of test samples: ', n_test_samples)
-    #test_sampler = SubsetRandomSampler(np.arange(1, n_test_samples+1, dtype=np.int64))
-    #test_loader = torch.utils.data.DataLoader(test_data_set, batch_size=batch_size, sampler=test_sampler, num_workers=4, **kwargs)
 
     print(' ')
 
--- BEV_network/train_network.py
+++ BEV_network/train_network.py
@@ -43,7 +43,7 @@ def train_network(n_epochs, learning_rate, patience, folder_path, use_cuda, batc
     print(' ')
 
     kwargs = {'pin_memory': True} if use_cuda else {}
-    train_loader = get_loaders(path_training_data, path_validation_data, path_test_data, batch_size, use_cuda, kwargs)
+    train_loader = get_loaders(path_training_data, path_validation_data, batch_size, use_cuda, kwargs)
 
     '''# Load weights
     if load_weights:
