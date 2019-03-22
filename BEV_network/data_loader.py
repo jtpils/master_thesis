085eca086c974738c48c203d5451b7c5b4a00c67
sablind <annlund@users.noreply.github.com>
2019-03-22 12:21:23 +0100

    test with lower resolution

--- BEV_network/data_loader.py
+++ BEV_network/data_loader.py
@@ -21,7 +21,7 @@ def get_loaders(path_training_data, path_validation_data, batch_size, use_cuda):
     n_training_samples = len(training_data_set)
     print('Number of training samples: ', n_training_samples)
     train_sampler = SubsetRandomSampler(np.arange(1, n_training_samples+1, dtype=np.int64))
-    train_loader = torch.utils.data.DataLoader(training_data_set, batch_size=batch_size, sampler=train_sampler, num_workers=8, **kwargs)
+    train_loader = torch.utils.data.DataLoader(training_data_set, batch_size=batch_size, sampler=train_sampler, num_workers=16, **kwargs)
     #train_loader = torch.utils.data.DataLoader(training_data_set, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=use_cuda)#**kwargs)
 
     # Validation
