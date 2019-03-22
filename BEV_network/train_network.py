875f7f6a2e5f5931daaff1f844eaf0b2c79095b0
sablind <annlund@users.noreply.github.com>
2019-03-22 12:17:05 +0100

    test with lower resolution

--- BEV_network/train_network.py
+++ BEV_network/train_network.py
@@ -88,7 +88,7 @@ def train_network(n_epochs, learning_rate, patience, folder_path, use_cuda, batc
 
 
         running_loss = 0.0
-        print_every = 10  #n_batches // 10  # how many mini-batches if we want to print stats x times per epoch
+        print_every = 1  #n_batches // 10  # how many mini-batches if we want to print stats x times per epoch
         start_time = time.time()
         total_train_loss = 0
 
