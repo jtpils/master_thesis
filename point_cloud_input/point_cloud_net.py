e79999997296b6e9f281069f84cdcab9f264500f
sablind <sablind@users.noreply.github.com>
2019-03-23 18:09:38 +0100

    in progress pointpillar

--- point_cloud_input/point_cloud_net.py
+++ point_cloud_input/point_cloud_net.py
@@ -26,7 +26,7 @@ class PointCloudNet(torch.nn.Module):
         out = self.fc2(combined)
 
 
-# TODO: THIS IS NOT TESTED AT ALL!
+# TODO: THIS IS NOT TESTED!
 class PFNLayer(torch.nn.Module):
     """
     Pillar Feature Net Layer (PFN) (The PFN could be composed of a series of these layers)
@@ -38,34 +38,22 @@ class PFNLayer(torch.nn.Module):
     :param use_norm: <bool>. Whether to include BatchNorm.
     :param last_layer: <bool>. If last_layer, there is no concatenation of features.
     """
-    def __init__(self,
-                 input_channels,
-                 output_channels,
-                 last_layer=False):
+
+    def __init__(self):
 
         super(PFNLayer, self).__init__()
 
-        self.last_vfe = last_layer
-        # This I do not understand from the pointpillar net // Sabina
-        #if not self.last_vfe:
-        #    out_channels = output_channels // 2
-        #self.units = out_channels
-
         # Linear Layer:  The linear layer can be formulated as a 1*1 convolution layer across the tensor
-        self.conv1 = torch.nn.Conv2d(8, 64, kernel_size=1, stride=0, padding=0)
+        self.conv1 = torch.nn.Conv2d(8, 64, kernel_size=1, stride=1, padding=0)
         # Batch Norm
         self.conv1_bn = torch.nn.BatchNorm2d(64)
 
     def forward(self, inputs):
 
         x = F.relu(self.conv1_bn(self.conv1(inputs)))
-        x = self.norm(x.permute(0, 2, 1).contiguous()).permute(0, 2, 1).contiguous()
-        x = F.relu(x)
-        x_max = torch.max(x, dim=1, keepdim=True)[0]
+        x_max = torch.max(x, dim=3, keepdim=True)[0]
 
-        if self.last_vfe:
-            return x_max
-        else:
-            x_repeat = x_max.repeat(1, inputs.shape[1], 1)
-            x_concatenated = torch.cat([x, x_repeat], dim=2)
-            return x_concatenated
+        return x_max
+
+    def name(self):
+        return "PFNLayer"
