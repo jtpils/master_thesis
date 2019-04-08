581e7d62afd567afeca2ce72f61b432fad30d32f
sablind <annlund@users.noreply.github.com>
2019-04-08 12:06:35 +0200

    train on cloud

--- BEV_network/train_samples_on_the_go.py
+++ BEV_network/train_samples_on_the_go.py
@@ -41,7 +41,7 @@ def get_loaders(batch_size, translation, rotation, use_cuda):
     kwargs = {'pin_memory': True} if use_cuda else {}
     workers_train = 16
     print('Number of workers: ', workers_train)
-    n_training_samples = len(training_data_set)
+    n_training_samples = 100 #len(training_data_set)
     print('Number of training samples: ', n_training_samples)
     train_sampler = SubsetRandomSampler(np.arange(n_training_samples, dtype=np.int64))
     train_loader = torch.utils.data.DataLoader(training_data_set, batch_size=batch_size, sampler=train_sampler, num_workers=workers_train, **kwargs)
@@ -60,7 +60,7 @@ def get_loaders(batch_size, translation, rotation, use_cuda):
     val_data_set = DataSetFakeData(sample_path, csv_path, translation, rotation)
 
     kwargs = {'pin_memory': True} if use_cuda else {}
-    n_val_samples = len(val_data_set)  # change so that this is 20% of all samples
+    n_val_samples = 100 #len(val_data_set)  # change so that this is 20% of all samples
     print('Number of validation samples: ', n_val_samples)
     val_sampler = SubsetRandomSampler(np.arange(n_val_samples, dtype=np.int64))
     val_loader = torch.utils.data.DataLoader(val_data_set, batch_size=batch_size, sampler=val_sampler, num_workers=workers_train, **kwargs)
