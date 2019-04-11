095ad21dce7dd4013993f60613d0c387516d143b
sablind <annlund@users.noreply.github.com>
2019-04-11 16:43:50 +0200

    trans and rot as input now, to avoid mistakes

--- BEV_on_the_go/main_test_set.py
+++ BEV_on_the_go/main_test_set.py
@@ -14,16 +14,26 @@ from torch.utils.data.sampler import SubsetRandomSampler
 
 
 load_weights = True
-path = '/home/master04/Desktop/network_parameters/Duchess_190411_2/'
-load_weights_path = path + 'parameters/epoch_15_checkpoint.pt'
+path = '/home/master04/Desktop/network_parameters/Gustav_190411_1'
+load_weights_path = os.path.join(path, 'parameters/epoch_9_checkpoint.pt')
 batch_size = 8
+translation = float(input('enter translation: '))
+rotation = float(input('enter rotation: '))
+
+f = open(os.path.join(path, 'outputs.txt'), "w+")
+f.close()
+with open(os.path.join(path, 'outputs.txt'), "a+") as f:
+    f.write("Metrics from test  \n \n")
+
 
 print('Number of GPUs available: ', torch.cuda.device_count())
 use_cuda = torch.cuda.is_available()
 print('CUDA available: ', use_cuda)
 
-CNN = Duchess()
+CNN = Gustav()
+print(' ')
 print('=======> NETWORK NAME: =======> ', CNN.name())
+print(' ')
 if use_cuda:
     CNN.cuda()
 print('Are model parameters on CUDA? ', next(CNN.parameters()).is_cuda)
@@ -38,30 +48,25 @@ if load_weights:
         CNN.load_state_dict(network_param['model_state_dict'])
 
 # ====== TEST SET =====
-#sample_path = '/home/master04/Desktop/Ply_files/validation_and_test/test_set/pc/'
-#csv_path = '/home/master04/Desktop/Ply_files/validation_and_test/test_set/test_set.csv'
-#grid_csv_path = '/home/master04/Desktop/Dataset/ply_grids/csv_grids_190409/csv_grids_test'
-
+sample_path = '/home/master04/Desktop/Ply_files/validation_and_test/test_set/pc/'
+csv_path = '/home/master04/Desktop/Ply_files/validation_and_test/test_set/test_set.csv'
+grid_csv_path = '/home/master04/Desktop/Dataset/ply_grids/csv_grids_190409/csv_grids_test'
 
 # ===== VALIDATION SET ======
-sample_path = '/home/master04/Desktop/Ply_files/validation_and_test/validation_set/pc/'
-csv_path = '/home/master04/Desktop/Ply_files/validation_and_test/validation_set/validation_set.csv'
-grid_csv_path = '/home/master04/Desktop/Dataset/ply_grids/csv_grids_190409/csv_grids_validation'
+#sample_path = '/home/master04/Desktop/Ply_files/validation_and_test/validation_set/pc/'
+#csv_path = '/home/master04/Desktop/Ply_files/validation_and_test/validation_set/validation_set.csv'
+#grid_csv_path = '/home/master04/Desktop/Dataset/ply_grids/csv_grids_190409/csv_grids_validation'
 
 kwargs = {'pin_memory': True, 'num_workers': 16} if use_cuda else {'num_workers': 8}
 
-
-test_set = DataSetMapData_createMapOnTheGo(sample_path, csv_path, grid_csv_path)
-n_test_samples = 1000 #len(test_set)
+test_set = DataSetMapData_createMapOnTheGo(sample_path, csv_path, grid_csv_path, translation=translation, rotation=rotation)
+n_test_samples = len(test_set)
 
 print('Number of test samples: ', n_test_samples)
 test_sampler = SubsetRandomSampler(np.arange(n_test_samples, dtype=np.int64))
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, sampler=test_sampler, **kwargs)
 
 CNN.eval()
-
-#predictions_list = list()
-#labels_list = list()
 predictions_array = np.zeros((1,3))
 labels_array = np.zeros((1,3))
 
@@ -106,44 +111,48 @@ with torch.no_grad():
         lab = labels.data.numpy()
         labels_array = np.concatenate((labels_array, lab), axis=0)
 
-        #predictions_list.append(outputs.data.tolist())
-        #labels_list.append(labels.data.tolist())
-
         if i%10 == 0:
             print('Batch ', i, ' of ', len(test_loader))
 
 predictions = predictions_array[1:,:]
 labels = labels_array[1:,:]
-#predictions = np.array(predictions_list)  # shape: (minibatch, batch_size, 3)
-#labels = np.array(labels_list)
-#predictions = predictions.reshape((n_test_samples, 3))  # shape (samples, 3)
-#labels = labels.reshape((n_test_samples, 3))
 diff = labels - predictions
 error_distances = np.hypot(diff[:,0], diff[:,1])
 
+
 def plot_histograms():
-    plt.subplot(1, 3, 1)
-    plt.hist(diff[:,0], bins='auto', label='Difference x')
-    plt.legend()
-    plt.ylabel('Difference in meters: x')
+    fig = plt.figure(figsize=(15,5))
+    fig.suptitle('Error distribution')
 
-    plt.subplot(1, 3, 2)
-    plt.hist(diff[:,1], bins='auto', label='Difference y')
-    plt.legend()
-    plt.ylabel('Difference in meters: y')
+    ax = fig.add_subplot(131)
+    ax.hist(diff[:,0], bins='auto', label='Difference x')
+    ax.set_xlabel('Difference in meters: x')
+    ax.set_ylabel('Number of samples')
+    ax.legend()
 
-    plt.subplot(1, 3, 3)
-    plt.hist(diff[:,2], bins='auto', label='Difference angle')
-    plt.legend()
-    plt.ylabel('Difference in degrees')
+    ax = fig.add_subplot(132)
+    ax.hist(diff[:,0], bins='auto', label='Difference y')
+    ax.set_xlabel('Difference in meters: y')
+    ax.set_ylabel('Number of samples')
+    ax.legend()
+
+    ax = fig.add_subplot(133)
+    ax.hist(diff[:,0], bins='auto', label='Difference angle')
+    ax.set_xlabel('Difference in degrees')
+    ax.set_ylabel('Number of samples')
+    ax.legend()
 
     plt.show()
+
+    file_name = 'errors.png'
+    fig.savefig(os.path.join(path, file_name))
 
 
 def visualize_samples():
     sorted = np.argsort(error_distances)
     # best samples
     print('==== Minimum error ====')
+    i=0
     for idx in sorted[:3]:
         data = test_set.__getitem__(idx)
         print('Error ', diff[idx,:])
@@ -152,13 +161,22 @@ def visualize_samples():
         print('Label distance ', np.hypot(labels[idx,0], labels[idx,1]))
         print(' ')
 
-        sample = data['sample']
-        plot_sample(sample[0,:,:], sample[1,:,:])
 
-        # visualize_detections
-        #visualize_detections(data['sample'],layer=0, fig_num=1)
-        #visualize_detections(data['sample'],layer=1, fig_num=2)
-        #plt.show()
+        with open(os.path.join(path, 'outputs.txt'), "a+") as f:
+            output_text = 'Sample ' + str(i) + '\n'
+            f.write(output_text)
+            output_text = 'Error ' + str(diff[idx,:]) + '\n'
+            f.write(output_text)
+            output_text = 'Error distance ' + str(error_distances[idx]) + '\n'
+            f.write(output_text)
+            output_text = 'Labels ' + str(labels[idx,:]) + '\n'
+            f.write(output_text)
+            output_text = 'Label distance ' + str(np.hypot(labels[idx,0], labels[idx,1])) + '\n \n'
+            f.write(output_text)
+
+        sample = data['sample']
+        plot_sample(sample[0,:,:], sample[1,:,:], i)
+        i = i+1
 
     print('==== Maximum error ====')
     for idx in sorted[-3:   ]:
@@ -169,27 +187,45 @@ def visualize_samples():
         print('Label distance ', np.hypot(labels[idx,0], labels[idx,1]))
         print(' ')
 
+
+        with open(os.path.join(path, 'outputs.txt'), "a+") as f:
+            output_text = 'Sample ' + str(i) + '\n'
+            f.write(output_text)
+            output_text = 'Error ' + str(diff[idx,:]) + '\n'
+            f.write(output_text)
+            output_text = 'Error distance ' + str(error_distances[idx]) + '\n'
+            f.write(output_text)
+            output_text = 'Labels ' + str(labels[idx,:]) + '\n'
+            f.write(output_text)
+            output_text = 'Label distance ' + str(np.hypot(labels[idx,0], labels[idx,1])) + '\n \n'
+            f.write(output_text)
+
         sample = data['sample']
-        plot_sample(sample[0,:,:], sample[1,:,:])
+        plot_sample(sample[0,:,:], sample[1,:,:], i)
+        i=i+1
 
-        #visualize_detections(data['sample'],layer=0, fig_num=1)
-        #visualize_detections(data['sample'],layer=1, fig_num=2)
-        #plt.show()
 
-'''
-def visualize_detections(discretized_point_cloud, layer=0, fig_num=1):
-    detection_layer = discretized_point_cloud[layer, :, :]
-    detection_layer[detection_layer > 0] = 255
-
-    plt.figure(fig_num)
-    plt.imshow(detection_layer, cmap='gray')
-'''
-
-def plot_sample(sweep_image, cutout_image):
+def plot_sample(sweep_image, cutout_image, fig_num):
 
     sweep_image[sweep_image > 0] = 255
     cutout_image[cutout_image > 0] = 255
-    plt.subplot(1,2,1)
+
+    fig = plt.figure()
+
+    ax = fig.add_subplot(121)
+    ax.imshow(sweep_image, cmap='gray')
+    ax.set_title('Sweep')
+    ax.axis('off')
+
+    ax = fig.add_subplot(122)
+    ax.imshow(cutout_image, cmap='gray')
+    ax.set_title('Map cut-out')
+    ax.axis('off')
+
+    file_name = 'sample' + str(fig_num) +'.png'
+    fig.savefig(os.path.join(path, file_name))
+
+    '''plt.subplot(1,2,1)
     plt.imshow(sweep_image, cmap='gray')
     plt.title('sweep')
     plt.axis('off')
@@ -197,53 +233,82 @@ def plot_sample(sweep_image, cutout_image):
     plt.imshow(cutout_image, cmap='gray')
     plt.title('map cut-out')
     plt.axis('off')
-    plt.show()
+    plt.show()'''
 
 
 def plot_loss():
-    #model_dict = torch.load(load_weights_path, map_location='cpu')
-    train = np.load(path + 'train_loss.npy')
-    val = np.load(path + 'val_loss.npy')
+    train = np.load(os.path.join(path,'train_loss.npy'))
+    val = np.load(os.path.join(path,'val_loss.npy'))
 
     train_batches = train[0]  #first element is the number of minibatches per epoch
     train_loss = train[1:]  #the following elements are the loss for each minibatch
-    #val_batches = val[0]
     val_loss = val[1:]
 
     num_epochs = len(train_loss) / train_batches
     train_vec = np.linspace(1, num_epochs, len(train_loss))
     val_vec = np.linspace(1, num_epochs, len(val_loss))
 
-    plt.plot(train_vec, train_loss, label='Training loss')
-    plt.plot(val_vec, val_loss, label='Validation loss')
-    plt.xlabel('Epochs')
-    plt.legend()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(train_vec, train_loss, label='Training loss')
+    ax.plot(val_vec, val_loss, label='Validation loss')
+    ax.set_xlabel('Epochs')
+    ax.set_ylabel('Loss per mini-batch')
+    ax.legend()
+
     plt.show()
+
+    file_name = 'losses.png'
+    fig.savefig(os.path.join(path, file_name))
 
 
 def plot_labels():
-    plt.subplot(1, 3, 1)
-    plt.hist(labels[:,0], bins='auto', label='Labels x')
-    plt.legend()
-    #plt.ylabel('Difference in meters: x')
+    fig = plt.figure(figsize=(15,5))
+    fig.suptitle('Label distribution')
 
-    plt.subplot(1, 3, 2)
-    plt.hist(labels[:,1], bins='auto', label='Labels y')
-    plt.legend()
-    #plt.ylabel('Difference in meters: y')
+    ax = fig.add_subplot(131)
+    ax.hist(labels[:,0], bins='auto', label='Labels x')
+    ax.legend()
 
-    plt.subplot(1, 3, 3)
-    plt.hist(labels[:,2], bins='auto', label='Labels angle')
-    plt.legend()
-    #plt.ylabel('Difference in degrees')
+    ax = fig.add_subplot(132)
+    ax.hist(labels[:,1], bins='auto', label='Labels y')
+    ax.legend()
+
+    ax = fig.add_subplot(133)
+    ax.hist(labels[:,2], bins='auto', label='Labels rotation')
+    ax.legend()
 
     plt.show()
+
+    file_name = 'labels.png'
+    fig.savefig(os.path.join(path, file_name))
+
+
+def save_as_png(image, file_name):
+    file_path = os.path.join(path, file_name+'.png')
+    '''plt.savefig('foo.png', bbox_inches='tight')
+
+    # create the png_path
+    png_path = folder_path_png + 'channel_' + str(channel)+'.png'
+
+    # Save images
+    img = Image.fromarray(discretized_pointcloud_BEV[channel, :, :])
+    new_img = img.convert("L")
+    new_img.rotate(180).save(png_path)'''
 
 
 def error_metrics():
     error_distances
     print('Error mean distance: ', np.mean(error_distances))
     print('Error median distance: ', np.median(error_distances))
+
+    with open(os.path.join(path, 'outputs.txt'), "a+") as f:
+        output_text = 'Error mean distance:  ' + str(np.mean(error_distances)) + '\n'
+        f.write(output_text)
+        output_text = 'Error median distance: ' + str(np.median(error_distances)) + '\n'
+        f.write(output_text)
+
+
 
 def main():
 
