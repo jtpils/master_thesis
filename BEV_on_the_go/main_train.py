0a1dcf782d132625db43636bed843be049e19c10
sablind <annlund@users.noreply.github.com>
2019-04-12 13:38:49 +0200

    h

--- BEV_on_the_go/main_train.py
+++ BEV_on_the_go/main_train.py
@@ -72,7 +72,7 @@ def main():
             print('batch_size', batch_size)
     if CNN.name() == 'Aslan':
         if use_cuda:
-            batch_size = 93
+            batch_size = 85
             print('batch_size', batch_size)
     print('=======> NETWORK NAME: =======> ', CNN.name())
     #if use_cuda:
